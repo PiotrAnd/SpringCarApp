92ec0f97e08940943bec0afe6a0941b8b66894e6
Piotr <cortezzz2o2.pl>
2020-03-02 13:00:17 +0100

    1

--- src/main/java/com/example/CarApp/repo/Car50aRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CarApp.repo;
-
-import com.example.CarApp.model.Car50a;
-import org.springframework.data.repository.CrudRepository;
-
-public interface Car50aRepository extends CrudRepository<Car50a, Integer> {
-}
